e845906cc0320af3011dbf0e2489422b597b9010
stiven trujillo <strujillodev@gmail.com>
2024-09-30 12:37:59 -0500

    feat: add tabs

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { SearchResponse, SuggestionsResponse } from "@/types";
 import { useMutation } from "@tanstack/react-query";
 import { OpenAI } from "openai";
@@ -54,7 +55,7 @@ function App() {
   >("success");
 
   // Image URL
-  const [url, setUrl] = useState("")
+  const [url, setUrl] = useState("");
 
   // Get Suggestions by term Query
   const { data: dataSuggestions, mutate: mutateSuggestions } = useMutation({
@@ -95,6 +96,7 @@ function App() {
   });
 
   async function onSubmit(formData: FormData) {
+    if (!formData.message) return;
     mutateSuggestions(formData.message);
     mutateSearch(formData.message);
     form.reset();
@@ -108,7 +110,7 @@ function App() {
     setDescriptionStatus("loading");
 
     try {
-      setUrl(data.url)
+      setUrl(data.url);
 
       const openai = new OpenAI({
         dangerouslyAllowBrowser: true,
@@ -163,12 +165,12 @@ function App() {
         apiKey: import.meta.env.VITE_OPENAI_API_KEY,
       });
       const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo", // gpt-4o-mini
+        model: "gpt-4o-mini", // gpt-4o-mini gpt-3.5-turbo
         messages: [
           {
             role: "system",
             content:
-              "Eres un experto generador de listas de títulos para productos destinados a plataformas de e-commerce como eBay, Amazon y MercadoLibre. Sigue las siguientes pautas: 1. Usa la mayor cantidad posible de palabras clave proporcionadas por el usuario. 2. Los títulos deben ser concisos, atractivos y optimizados para mejorar la visibilidad en motores de búsqueda (SEO). 3. No incluyas signos de puntuación, conectores, conjunciones, preposiciones, tildes, guiones ni caracteres especiales. 4. Los títulos deben estar formados por 8 a 12 palabras clave relevantes, utilizando sinónimos cuando sea necesario. 5. Cada título debe estar separado por un salto de línea obligatorio. 6. El último titulo debe ser obligatoriamente una combinación de todos los títulos generados sin limite de palabras",
+              "Eres un experto generador de listas de títulos para productos destinados a plataformas de e-commerce como eBay, Amazon y MercadoLibre. Sigue las siguientes pautas: 1. Usa la mayor cantidad posible de palabras clave proporcionadas por el usuario. 2. Los títulos deben ser concisos, atractivos y optimizados para mejorar la visibilidad en motores de búsqueda (SEO). 3. No incluyas signos de puntuación, conectores, conjunciones, preposiciones, tildes, guiones ni caracteres especiales. 4. Los títulos deben estar formados por 8 a 12 palabras clave relevantes, utilizando sinónimos cuando sea necesario. 5. Cada título debe estar separado por un salto de línea obligatorio.",
           },
           {
             role: "user",
@@ -196,166 +198,185 @@ function App() {
       <h1 className="text-2xl text-center py-2 font-bold">
         Vendedores Mercadolibre Pro
         <span className="text-xs block font-light">
-          Generador de títulos de productos
+          Generador de títulos y descripciones de productos
         </span>
       </h1>
       <div className="w-full max-w-md mx-auto space-y-4">
-        {/* term form */}
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-5 pb-2"
-          >
-            <FormField
-              control={form.control}
-              name="message"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Termino de Búsqueda</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Que deseas buscar?..." {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    Este es tu termino de búsqueda para buscar productos en ML.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit" className="uppercase">
-              Consultar
-            </Button>
-          </form>
-        </Form>
+        <Tabs defaultValue="account" className="w-full">
+          <TabsList className="w-full">
+            <TabsTrigger value="account">Títulos Productos</TabsTrigger>
+            <TabsTrigger value="password">Descripciones Productos</TabsTrigger>
+          </TabsList>
+          <TabsContent value="account">
+            <div className="space-y-4">
+              {/* term form */}
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-5 pb-2"
+                >
+                  <FormField
+                    control={form.control}
+                    name="message"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Termino de Búsqueda</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Que deseas buscar?..."
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormDescription>
+                          Este es tu termino de búsqueda para buscar productos
+                          en ML.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit" className="uppercase">
+                    Consultar
+                  </Button>
+                </form>
+              </Form>
 
-        {/* suggestions card */}
-        <Card>
-          <CardHeader>
-            <CardTitle>Sugerencias</CardTitle>
-            <CardDescription>
-              Estas son las sugerencias de búsqueda generadas por ML.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            {dataSuggestions &&
-              dataSuggestions.suggested_queries.map(
-                (suggested_query, index) => (
-                  <p key={index}>{suggested_query.q}</p>
-                )
-              )}
-          </CardContent>
-        </Card>
+              {/* suggestions card */}
+              <Card>
+                <CardHeader>
+                  <CardTitle>Sugerencias</CardTitle>
+                  <CardDescription>
+                    Estas son las sugerencias de búsqueda generadas por ML.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  {dataSuggestions &&
+                    dataSuggestions.suggested_queries.map(
+                      (suggested_query, index) => (
+                        <p key={index}>{suggested_query.q}</p>
+                      )
+                    )}
+                </CardContent>
+              </Card>
 
-        {/* Searches card */}
-        <Card>
-          <CardHeader>
-            <CardTitle>Búsquedas</CardTitle>
-            <CardDescription>
-              Estos son los títulos de los productos encontrados en ML.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            {dataSearch &&
-              dataSearch.results.map((result) => (
-                <p key={result.id} className="py-2">
-                  {result.title}{" "}
-                  <span className="text-xs">
-                    ({result.title.length} caracteres)
-                  </span>
-                </p>
-              ))}
-          </CardContent>
-        </Card>
+              {/* Searches card */}
+              <Card>
+                <CardHeader>
+                  <CardTitle>Búsquedas</CardTitle>
+                  <CardDescription>
+                    Estos son los títulos de los productos encontrados en ML.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  {dataSearch &&
+                    dataSearch.results.map((result) => (
+                      <p key={result.id} className="py-2">
+                        {result.title}{" "}
+                        <span className="text-xs">
+                          ({result.title.length} caracteres)
+                        </span>
+                      </p>
+                    ))}
+                </CardContent>
+              </Card>
 
-        {/* suggested product title card */}
-        <Card>
-          <CardHeader>
-            <CardTitle>Titulo de Producto Sugerido</CardTitle>
-            <CardDescription>
-              Estas son las sugerencias generadas por IA para los títulos de los
-              productos en ML.
-            </CardDescription>
-            {/* Button Suggest Titles */}
-            <Button
-              className="uppercase"
-              type="button"
-              onClick={handleSuggestTitles}
-              // disabled={!dataSuggestions && !dataSearch}
-            >
-              Generar Sugerencias de Títulos
-            </Button>
-          </CardHeader>
-          <CardContent>
-            {status === "loading" && <p>Cargando...</p>}
-            {status === "error" && <p>Error...</p>}
-            {status === "success" &&
-              response &&
-              response.split("\n").map((title, index) => {
-                if (title)
-                  return (
-                    <div className="p-3" key={index}>
-                      <p>{title.trim()} </p>
-                      <p className="text-xs">({title.length} caracteres)</p>
-                    </div>
-                  );
-              })}
-          </CardContent>
-        </Card>
+              {/* suggested product title card */}
+              <Card>
+                <CardHeader>
+                  <CardTitle>Titulo de Producto Sugerido</CardTitle>
+                  <CardDescription>
+                    Estas son las sugerencias generadas por IA para los títulos
+                    de los productos en ML.
+                  </CardDescription>
+                  {/* Button Suggest Titles */}
+                  <Button
+                    className="uppercase"
+                    type="button"
+                    onClick={handleSuggestTitles}
+                    // disabled={!dataSuggestions && !dataSearch}
+                  >
+                    Generar Sugerencias de Títulos
+                  </Button>
+                </CardHeader>
+                <CardContent>
+                  {status === "loading" && <p>Cargando...</p>}
+                  {status === "error" && <p>Error...</p>}
+                  {status === "success" &&
+                    response &&
+                    response.split("\n").map((title, index) => {
+                      if (title)
+                        return (
+                          <div className="p-3" key={index}>
+                            <p>{title.trim()} </p>
+                            <p className="text-xs">
+                              ({title.length} caracteres)
+                            </p>
+                          </div>
+                        );
+                    })}
+                </CardContent>
+              </Card>
+            </div>
+          </TabsContent>
+          <TabsContent value="password">
+            {/* Generate Description Form */}
+            <Form {...descriptionForm}>
+              <form
+                onSubmit={descriptionForm.handleSubmit(
+                  handleGenerateDescription
+                )}
+                className="space-y-5 pb-2"
+              >
+                <FormField
+                  control={descriptionForm.control}
+                  name="url"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>URL imagen del Producto</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="URL imagen del producto..."
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription>
+                        Genera una descripción para el producto.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit" className="uppercase">
+                  Generar Descripción
+                </Button>
+              </form>
+            </Form>
 
-        {/* Generate Description Form */}
-        <Form {...descriptionForm}>
-          <form
-            onSubmit={descriptionForm.handleSubmit(handleGenerateDescription)}
-            className="space-y-5 pb-2"
-          >
-            <FormField
-              control={descriptionForm.control}
-              name="url"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>URL imagen del Producto</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="URL imagen del producto..."
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormDescription>
-                    Genera una descripción para el producto.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit" className="uppercase">
-              Generar Descripción
-            </Button>
-          </form>
-        </Form>
+            {/* Image */}
+            <div className="flex justify-center">
+              {url && <img src={url} alt="product" className="w-1/3" />}
+            </div>
 
-        {/* Image */}
-        <div className="flex justify-center">
-          {url && <img src={url} alt="product" className="w-1/3" />}
-        </div>
-
-        {/* Generate Description card */}
-        <Card>
-          <CardHeader>
-            <CardTitle>Descripción de Producto Generada</CardTitle>
-            <CardDescription>
-              Esta es la descripción generada por IA para el producto.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            {descriptionStatus === "loading" && <p>Cargando...</p>}
-            {descriptionStatus === "error" && <p>Error...</p>}
-            {descriptionStatus === "success" && descriptionResponse && (
-              <Markdown remarkPlugins={[remarkGfm]} className="space-y-4">
-                {descriptionResponse}
-              </Markdown>
-            )}
-          </CardContent>
-        </Card>
+            {/* Generate Description card */}
+            <Card>
+              <CardHeader>
+                <CardTitle>Descripción de Producto Generada</CardTitle>
+                <CardDescription>
+                  Esta es la descripción generada por IA para el producto.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                {descriptionStatus === "loading" && <p>Cargando...</p>}
+                {descriptionStatus === "error" && <p>Error...</p>}
+                {descriptionStatus === "success" && descriptionResponse && (
+                  <Markdown remarkPlugins={[remarkGfm]} className="space-y-4">
+                    {descriptionResponse}
+                  </Markdown>
+                )}
+              </CardContent>
+            </Card>
+          </TabsContent>
+        </Tabs>
       </div>
     </main>
   );
